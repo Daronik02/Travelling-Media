6d3add4894414b33fdfa0642dd35a8bdb581a277
Sumanta Dey <116909777+Daronik02@users.noreply.github.com>
2025-06-15 23:44:10 +0530

    Delete Server directory

--- Server/routes/posts.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createPost, getPosts } = require("../controllers/postController");
-
-router.post("/", createPost);
-router.get("/", getPosts);
-
-module.exports = router;
